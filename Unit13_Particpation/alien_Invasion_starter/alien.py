a814879946fa788fac4a19e580d7c86d50fc245e
zostheimer-cscc <zostheimer@student.cscc.edu>
2026-04-19 23:22:59 -0400

    Commit 2
    Updated alien.py and settings.py  again

--- Unit13_Particpation/alien_Invasion_starter/alien.py
+++ Unit13_Particpation/alien_Invasion_starter/alien.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
 
+
 class Alien(Sprite):
     def __init__(self, game: 'AlienInvasion', x: float, y: float) -> None:
         super().__init__()
@@ -22,10 +23,22 @@ class Alien(Sprite):
         self.rect.x = x
         self.rect.y = y
 
-        # self.y = float(self.rect.y)
+        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
     def update(self) -> None:
-        pass
+        temp_speed = self.settings.fleet_speed
 
-    def draw_alien(self) -> None;
+        if self.check_edges():
+            self.settings.fleet_direction *= -1
+            self.y += self.settings.fleet_drop_speed
+
+        self.x += temp_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def check_edges(self) -> bool:
+        return (self.rect.right >= self.boundaries.right or self.rect.left <= self.boundaries.left)
+
+    def draw_alien(self) -> None:
         self.screen.blit(self.image, self.rect)
--- Unit13_Particpation/alien_Invasion_starter/settings.py
+++ Unit13_Particpation/alien_Invasion_starter/settings.py
@@ -21,3 +21,8 @@ class Settings:
         self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 5
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 40
